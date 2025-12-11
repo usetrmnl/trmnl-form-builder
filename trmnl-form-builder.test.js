e64d16688a01bea9257f659d48db6f77e663ce36
Mario Lurig <ucffool@gmail.com>
2025-12-10 22:01:02 -0700

    switched to pnpm

--- trmnl-form-builder.test.js
+++ trmnl-form-builder.test.js
@@ -1,7 +1,8 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll } from 'vitest'
+import './trmnl-form-builder.js'
 
 describe('TRMNL Form Builder', () => {
-  it('should be defined', () => {
+  it('should register the custom element', () => {
     expect(customElements.get('trmnl-form-builder')).toBeDefined()
   })
 })
